0c413fc4039838215cccc4e37711c0af9140f6a8
[SW5]엄태호 <sisofiy6048@gmail.com>
2023-08-17 18:06:59 +0900

    Feat: 헤더 컴포넌트 및 메인페이지에 적용

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Header from '@/components/header/Header';
 
 const MainPage = () => {
-  return <h1>메인 페이지입니다.</h1>;
+  return (
+    <div>
+      <Header isMainPage />
+      <h1>메인 페이지입니다.</h1>
+    </div>
+  );
 };
 
 export default MainPage;

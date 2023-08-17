8e7356237481f426964b56facff894a155451eae
[SW5]엄태호 <sisofiy6048@gmail.com>
2023-08-17 21:25:13 +0900

    Feat: 푸터 컴포넌트 및 메인페이지에 적용

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '@/components/header/Header';
+import Footer from '@/components/footer/Footer';
 
 const MainPage = () => {
   return (
     <div>
       <Header isMainPage />
       <h1>메인 페이지입니다.</h1>
+      <Footer />
     </div>
   );
 };

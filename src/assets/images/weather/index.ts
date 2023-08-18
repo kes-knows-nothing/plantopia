e46c3de804cd2e8188856a0050578d0761393fa2
[SW5]엄태호 <sisofiy6048@gmail.com>
2023-08-18 14:11:26 +0900

    Design: 날씨 관련 이미지 추가

---
+++ src/assets/images/weather/index.ts
@@ -0,0 +1,23 @@
+import CLOUD from './cloud.png';
+import DUST_STORM from './dust_storm.png';
+import FOG from './fog.png';
+import RAIN from './rain.png';
+import SHOWER from './shower.png';
+import SNOW from './snow.png';
+import STORM from './storm.png';
+import SUN_CLOUD from './sun_cloud.png';
+import SUN from './sun.png';
+import THUNDER from './thunder.png';
+
+export default {
+  CLOUD,
+  DUST_STORM,
+  FOG,
+  RAIN,
+  SHOWER,
+  SNOW,
+  STORM,
+  SUN_CLOUD,
+  SUN,
+  THUNDER,
+};

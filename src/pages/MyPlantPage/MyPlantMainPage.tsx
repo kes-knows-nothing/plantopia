4fb785bb84f82cc97d4ed05b1bab95d01f07d871
kes-knows-nothing <allinpositive@gmail.com>
2023-08-23 00:31:22 +0900

    completed-my-plant-main-data

--- src/pages/MyPlantPage/MyPlantMainPage.tsx
+++ src/pages/MyPlantPage/MyPlantMainPage.tsx
@@ -1,9 +1,23 @@
+import { useState, useEffect } from 'react';
 import './myPlantMainPage.scss';
 import Header from '@/components/header/Header';
 import Footer from '@/components/footer/Footer';
 import ellipseImage from './img/Ellipse_200.png';
 import plusIcon from '@/assets/images/icons/ph_plus-light.png';
 import MyPlantList from '@/pages/MyPlantPage/components/SubPlantList';
+
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '@/utils/firebaseApp';
+
+// interface MyPlantProps {
+//   frequency: number,
+//   imgUrl: string,
+//   isMain: boolean,
+//   nickname: string,
+//   plantName: string,
+//   purchasedDay:
+
+// }
 
 const dummyData = [
   {
@@ -14,6 +28,22 @@ const dummyData = [
 ];
 
 const MyPlantMainPage = () => {
+  const [myPlantData, setMyPlantData] = useState();
+
+  useEffect(() => {
+    const docRef = doc(db, 'plant', 'zEpCaZp6bG55uRSckG6h');
+    const getDocSnap = async () => {
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        console.log('Document data:', docSnap.data());
+        const data = docSnap.data();
+        setMyPlantData(data);
+      } else {
+        console.log('No such document!');
+      }
+    };
+    getDocSnap();
+  }, []);
   return (
     <>
       <Header />

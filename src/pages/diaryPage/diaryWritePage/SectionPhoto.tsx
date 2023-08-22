21dad352819f7d5bbe04340278ce6d986f86d34e
joy <leejoys.lab@gmail.com>
2023-08-23 02:50:18 +0900

    Feat: 다이어리 글쓰기 사진첨부 슬라이드 작업

--- src/pages/diaryPage/diaryWritePage/SectionPhoto.tsx
+++ src/pages/diaryPage/diaryWritePage/SectionPhoto.tsx
@@ -1,106 +1,90 @@
-import React, { useState, useRef, useCallback, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
-import { TbCameraPlus } from 'react-icons/tb';
 import { RiCloseFill } from 'react-icons/ri';
 import 'swiper/css';
 
 const SectionPhoto = () => {
-  const [slidesPerView, setSlidesPerView] = useState(3.5);
-  const [slides, setSlides] = useState(
-    new Array(4).fill({ backgroundImage: '' }),
-  );
-  const uploadButtonRefs = useRef([]);
+  const [slidesPerView, setSlidesPerView] = useState(3);
+  const [imgUrls, setImgUrls] = useState([]); 
+  const [currentCount, setCurrentCount] = useState(0);
 
+  /* 반응형 설정 */
   useEffect(() => {
     const handleResize = () => {
-      setSlidesPerView(window.innerWidth > 768 ? 4 : 3.5);
+      setSlidesPerView(window.innerWidth > 768 ? 4 : 2.5);
     };
-
     handleResize();
-
     window.addEventListener('resize', handleResize);
+
     return () => {
       window.removeEventListener('resize', handleResize);
     };
   }, []);
 
-  const handleFileSelect = useCallback(
-    (event, index) => {
-      const file = event.target.files[0];
-      if (file) {
-        const reader = new FileReader();
-        reader.onload = e => {
-          const backgroundImage = `url(${e.target.result})`;
-          const updatedSlides = [...slides];
-          updatedSlides[index] = { backgroundImage };
-          uploadButtonRefs.current[index].style.backgroundImage =
-            backgroundImage;
-          setSlides(updatedSlides);
-        };
-        reader.readAsDataURL(file);
-      }
-      event.target.value = null;
-    },
-    [slides],
-  );
+  const handleFileSelect = event => {
+    const file = event.target.files[0];
+    if (file && currentCount < 4) {
+      const reader = new FileReader();
+      reader.onload = e => {
+        const backgroundImage = `url(${e.target.result})`;
+        const updatedImgUrls = [...imgUrls, { backgroundImage }];
+        setImgUrls(updatedImgUrls);
+        setCurrentCount(currentCount + 1);
+      };
+      reader.readAsDataURL(file);
+    }
+    event.target.value = null;
+  };
 
-  const handleDeleteFile = useCallback(
-    index => {
-      const updatedSlides = [...slides];
-      updatedSlides[index] = { backgroundImage: '' };
-      setSlides(updatedSlides);
-      uploadButtonRefs.current[index].style.backgroundImage = '';
-    },
-    [slides],
-  );
+  const handleDeleteFile = index => {
+    const updatedImgUrls = imgUrls.filter((_, i) => i !== index);
+    setImgUrls(updatedImgUrls);
+    setCurrentCount(currentCount - 1);
+  };
 
   return (
-    <section className="photo_section">
+    <section className="photo_section inner">
+      {currentCount < 4 && (
+        <div className="upload_button_wrapper">
+          <button className="upload_button">
+            <label htmlFor="photoInput" className="photo_label">
+              <div className="photo_counter">
+                <span className="current_count">{currentCount}</span>
+                <span>/</span>
+                <span className="max_count">4</span>
+              </div>
+            </label>
+            <input
+              className="photo_input"
+              id="photoInput"
+              accept="image/*"
+              type="file"
+              onChange={handleFileSelect}
+            />
+          </button>
+        </div>
+      )}
       <Swiper
-        className="photo_select_swiper"
+        className={`photo_select_swiper ${
+          currentCount === 4 ? 'full_photo' : ''
+        }`}
         modules={[Navigation]}
         slidesPerView={slidesPerView}
-        spaceBetween={0}
       >
-        {slides.map((slide, index) => (
-          <SwiperSlide
-            key={index}
-            className={`slide ${index === 0 ? 'first_slide' : ''}`}
-          >
-            <button
-              className={`upload_button ${
-                slide.backgroundImage ? 'attached' : ''
-              }`}
-              ref={el => (uploadButtonRefs.current[index] = el)}
+        {imgUrls.map((url, index) => (
+          <SwiperSlide key={index} className="slide">
+            <div
+              className={`photo_slide attached`}
+              style={{
+                backgroundImage: url.backgroundImage,
+              }}
             >
-              <label htmlFor={`photoInput-${index}`} className="photo_label">
-                <div className="photo_text">
-                  {!slide.backgroundImage && (
-                    <>
-                      <TbCameraPlus className="camera_icon" />
-
-                      <div className="photo_counter hide">
-                        <span className="current_count">{index}</span>
-                        <span>/</span>
-                        <span className="max_count">4</span>
-                      </div>
-                    </>
-                  )}
-                </div>
-              </label>
-              <input
-                className="photo_input"
-                id={`photoInput-${index}`}
-                accept="image/*"
-                type="file"
-                onChange={e => handleFileSelect(e, index)}
-              />
-              {index === 0 && slide.backgroundImage && (
+              {index === 0 && url.backgroundImage && (
                 <div className="main_photo">대표사진</div>
               )}
-            </button>
-            {slide.backgroundImage && (
+            </div>
+            {url.backgroundImage && (
               <button
                 className="photo_delete_btn"
                 onClick={() => handleDeleteFile(index)}

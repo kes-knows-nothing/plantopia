a164514892d5303d3a66ccc44b8b40603386c6f3
Park ChangHyub <thwlckd@naver.com>
2023-08-25 15:30:37 +0900

    Feat: 식물도감 페이지 추천 카테고리별 랜덤 식물 8개 띄우는 작업 - orderby(target: 4가지, direction: 2가지)를 이용해 추천 카테고리별 8(4x2)가지 추천 식물 리스트 랜덤 제공 - firestore 복합 인덱스 추가 - 추천 식물 리스트 셔플링

--- src/pages/dictPage/Recommend.tsx
+++ src/pages/dictPage/Recommend.tsx
@@ -1,7 +1,15 @@
 import { useState, useEffect, Children } from 'react';
 import { Link } from 'react-router-dom';
 import { db } from '@/utils/firebaseApp';
-import { collection, getDocs, query, where, limit } from 'firebase/firestore';
+import {
+  collection,
+  getDocs,
+  query,
+  where,
+  limit,
+  orderBy,
+  OrderByDirection,
+} from 'firebase/firestore';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Navigation } from 'swiper/modules';
 import { mockData } from '@/mock/dictMock';
@@ -17,7 +25,7 @@ export interface PlantType {
   imageUrl: string;
   adviseInfo: string;
   blightInfo: string[];
-  growCode: keyof typeof codeInfo;
+  growCode: string;
   humidityCode: keyof typeof codeInfo;
   lightCode: keyof typeof codeInfo;
   recommendCode: keyof typeof codeInfo;
@@ -47,33 +55,49 @@ export const TargetQuery = {
   dark: ['lightCode', 'LC01'],
 };
 
+const orderDirection: OrderByDirection[] = ['asc', 'desc'];
+
 const Recommend = ({ icon, title, target }: RecommendProps) => {
   const [plant, setPlant] = useState<PlantType[]>([]);
 
+  const getDouments = async (target: keyof typeof TargetQuery) => {
+    const dictRef = collection(db, 'dictionary');
+    const q = query(
+      dictRef,
+      where(TargetQuery[target][0], '==', TargetQuery[target][1]),
+      orderBy(
+        Object.values(TargetQuery)[getRandomIndex(4)][0],
+        orderDirection[getRandomIndex(2)],
+      ),
+      limit(8),
+    );
+    const querySnapshot = await getDocs(q);
+    const queriedData: PlantType[] = [];
+    querySnapshot.forEach(doc => {
+      queriedData.push(doc.data() as PlantType);
+    });
+    setPlant(prev => [...prev, ...shuffleArray(queriedData)]);
+  };
+
+  const getMockData = async () => {
+    shuffleArray(mockData);
+    mockData.map(item => setPlant(prev => [...prev, item] as PlantType[]));
+  };
+
+  const getRandomIndex = (length: number) => {
+    return Math.floor(Math.random() * length);
+  };
+
+  const shuffleArray = (array: PlantType[]) => {
+    return array.sort(() => Math.random() - 0.5);
+  };
+
   useEffect(() => {
-    // Mock Data 사용시 아래 주석 처리
-    const getDouments = async (target: keyof typeof TargetQuery) => {
-      const dictRef = collection(db, 'dictionary');
-      const q = query(
-        dictRef,
-        where(TargetQuery[target][0], '==', TargetQuery[target][1]),
-        limit(8),
-      );
-      const querySnapshot = await getDocs(q);
-      querySnapshot.forEach(doc => {
-        setPlant(prev => {
-          const data = doc.data();
-          return [...prev, data] as PlantType[];
-        });
-      });
-    };
+    // Mock Data 사용시 getDouments 주석 처리
     getDouments(target);
 
-    // Mock Data 사용시 아래 주석 해제
-    // const getDouments = async () => {
-    //   mockData.map(item => setPlant(prev => [...prev, item] as PlantType[]));
-    // };
-    // getDouments();
+    // Mock Data 사용시 getMockData 주석 해제
+    // getMockData();
   }, []);
 
   return (

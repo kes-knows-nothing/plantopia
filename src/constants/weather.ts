1c78289e72ff6a7b0869ed719b1f93bcc622d3e9
[SW5]엄태호 <sisofiy6048@gmail.com>
2023-08-26 13:59:10 +0900

    Refactor: 날씨 컨텐츠 객체 분리

---
+++ src/constants/weather.ts
@@ -0,0 +1,55 @@
+import weather from '@/assets/images/weather';
+
+interface WeatherContentType {
+  imgSrc: string;
+  title: string;
+  content: string;
+}
+
+export const weatherContents: { [code: string]: WeatherContentType } = {
+  200: {
+    imgSrc: weather.THUNDER,
+    title: '천둥번개',
+    content: '집에서 영화나 책을 보는 건 어떨까요?',
+  },
+  300: {
+    imgSrc: weather.RAIN,
+    title: '한때 비',
+    content: '오늘은 창밖으로 빗소리가 들리겠어요.',
+  },
+  500: {
+    imgSrc: weather.SHOWER,
+    title: '비',
+    content: '실내에서 빗소리를 감상해보세요.',
+  },
+  600: {
+    imgSrc: weather.SNOW,
+    title: '눈',
+    content: '눈사람을 만들어보는건 어떨까요?',
+  },
+  800: {
+    imgSrc: weather.SUN,
+    title: '맑음',
+    content: '맑은 날, 기분 좋은 하루를 보내세요.',
+  },
+  801: {
+    imgSrc: weather.SUN_CLOUD,
+    title: '구름 조금',
+    content: '가볍게 산책하며 시간을 보내보세요.',
+  },
+  802: {
+    imgSrc: weather.SUN_CLOUD,
+    title: '구름 조금',
+    content: '가볍게 산책하며 시간을 보내보세요.',
+  },
+  803: {
+    imgSrc: weather.CLOUD,
+    title: '흐림',
+    content: '음악을 들으며 여유를 느껴보세요.',
+  },
+  804: {
+    imgSrc: weather.CLOUD,
+    title: '흐림',
+    content: '음악을 들으며 여유를 느껴보세요.',
+  },
+};

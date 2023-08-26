1284feb2cae53a2e5abc2e279dbec214ae47d64e
joy <leejoys.lab@gmail.com>
2023-08-27 02:14:50 +0900

    Refactor: 리스트뷰, 갤러리뷰 코드리뷰 수정사항 반영

--- src/pages/diaryPage/ListView.tsx
+++ src/pages/diaryPage/ListView.tsx
@@ -1,58 +1,72 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
+const DiaryItem = ({ diary, handleDelete }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-const ListView = ({ diaryData, handleDelete }) => {
-  const getImageClassName = imgUrls => {
-    if (!imgUrls) return '';
-    if (imgUrls.length === 0) return 'hide';
-    if (imgUrls.length > 1) return 'many';
-    return '';
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
   };
 
-  const [openModalIndex, setOpenModalIndex] = useState(null);
-
-  const toggleModal = index => {
-    setOpenModalIndex(prevIndex => (prevIndex === index ? null : index));
+  const closeModal = () => {
+    setIsModalOpen(false);
   };
 
   return (
+    <li className="diary_list">
+      <Link to={`/diary/${diary.id}`}>
+        <div className="left_box">
+          <h5 className="title">{diary.title}</h5>
+          <p className="content">{diary.content}</p>
+          <span className="date">
+            {diary.postedAt.toDate().toLocaleDateString()}
+          </span>
+        </div>
+        <div
+          className={`main_img ${diary.imgUrls.length === 0 ? 'hide' : 'show'} ${
+            diary.imgUrls.length > 1 ? 'many' : ''
+          }`}
+          style={{
+            backgroundImage: `url('${
+              diary.imgUrls && diary.imgUrls.length > 0
+                ? diary.imgUrls[0]
+                : ''
+            }')`,
+          }}
+        ></div>
+      </Link>
+      <button className="more" onClick={toggleModal}></button>
+      {isModalOpen && (
+        <Modal handleDelete={handleDelete} closeModal={closeModal} />
+      )}
+    </li>
+  );
+};
+
+const Modal = ({ handleDelete, closeModal }) => {
+  return (
+    <div className="more_modal">
+      <div className="btn modify">게시글 수정</div>
+      <div className="btn delete" onClick={() => {
+        handleDelete();
+        closeModal();
+      }}>
+        삭제
+      </div>
+    </div>
+  );
+};
+
+const ListView = ({ diaryData, handleDelete }) => {
+  return (
     <div className="list_view">
       <ul className="diary_list_wrap">
-        {diaryData.map((d, index) => (
-          <li className="diary_list" key={index}>
-            <Link to={`/diary/detail`}>
-              <div className="left_box">
-                <h5 className="title">{d.title}</h5>
-                <p className="content">{d.content}</p>
-                <span className="date">
-                  {d.postedAt.toDate().toLocaleDateString()}
-                </span>
-              </div>
-              <div
-                className={`main_img ${getImageClassName(d.imgUrls)}`}
-                style={{
-                  backgroundImage: `url('${
-                    d.imgUrls && d.imgUrls.length > 0
-                      ? d.imgUrls[0]
-                      : ''
-                  }')`,
-                }}
-              ></div>
-            </Link>
-            <button
-              className="more"
-              onClick={() => toggleModal(index)}
-            ></button>
-            {openModalIndex === index && (
-              <div className="more_modal">
-                <div className="btn modify">게시글 수정</div>
-                <div className="btn delete" onClick={() => handleDelete(index)}>
-                  삭제
-                </div>
-              </div>
-            )}
-          </li>
+        {diaryData.map((diary, index) => (
+          <DiaryItem
+            key={index}
+            diary={diary}
+            handleDelete={() => handleDelete(index)}
+          />
         ))}
       </ul>
     </div>

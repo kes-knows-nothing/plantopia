cfc64bb4f4b078348b9c0d0048ecb963957ee69e
joy <leejoys.lab@gmail.com>
2023-08-27 02:12:41 +0900

    Refactor: 글쓰기 페이지 코드리뷰 수정사항 반영

--- src/pages/diaryPage/diaryWritePage/SectionPhoto.tsx
+++ src/pages/diaryPage/diaryWritePage/SectionPhoto.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { storage } from '@/utils/firebaseApp';
 import {
   ref,
@@ -9,12 +9,13 @@ import {
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
 import 'swiper/css';
+import './sectionPhoto.scss'
 
 const SectionPhoto: React.FC<{
   userId: string;
   imgUrls: string[];
   setImgUrls: React.Dispatch<React.SetStateAction<string[]>>;
-}> = ({ userId, imgUrls, setImgUrls }) => {
+}> = ({ imgUrls, setImgUrls }) => {
   const [previewImgs, setPreviewImgs] = useState<{ backgroundImage: string }[]>(
     [],
   );

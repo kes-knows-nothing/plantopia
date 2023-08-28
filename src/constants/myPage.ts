84f2a2f4f84580f98efb504725ab789017949878
Park ChangHyub <thwlckd@naver.com>
2023-08-28 23:12:54 +0900

    Chore: 마이페이지 상수 코드 분리

---
+++ src/constants/myPage.ts
@@ -0,0 +1,7 @@
+const customerService = [
+  { title: '공지사항', url: '' },
+  { title: '자주 묻는 질문', url: '' },
+  { title: '식물 추가 요청', url: 'https://forms.gle/g4AjkNKqVDP48Xnc7' },
+];
+
+export { customerService };

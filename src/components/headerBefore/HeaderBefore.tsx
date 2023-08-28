e506d15d6b346b5f4d053dc25c1600673b58985b
Park ChangHyub <thwlckd@naver.com>
2023-08-28 18:37:17 +0900

    Feat: 버튼 누르면 이전 페이지 이동, 버튼 패딩 추가

--- src/components/headerBefore/HeaderBefore.tsx
+++ src/components/headerBefore/HeaderBefore.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import './headerBefore.scss';
 import BACK_ICON from '@/assets/images/icons/back_icon.png';
 import EX_ICON from '@/assets/images/icons/ex_icon.png';
@@ -8,9 +9,15 @@ interface HeaderBeforeProps {
 }
 
 const HeaderBefore = ({ ex, title }: HeaderBeforeProps) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(-1);
+  };
+
   return (
     <header className="header_container inner">
-      <button className={ex ? 'ex_btn' : 'back_btn'}>
+      <button className={ex ? 'ex_btn' : 'back_btn'} onClick={handleClick}>
         <img src={ex ? EX_ICON : BACK_ICON} />
       </button>
       <h2 className="header_title">{title}</h2>

1a5f6bc218f08d48a8513a1d5251b30e64d210ad
[SW5]엄태호 <sisofiy6048@gmail.com>
2023-08-28 23:19:39 +0900

    Refactor: 캘린더 모킹데이터 별도 파일로 분리

--- src/pages/calendarPage/CalendarPage.tsx
+++ src/pages/calendarPage/CalendarPage.tsx
@@ -1,98 +1,23 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Calendar from 'react-calendar';
-import { db, auth } from '@/firebaseApp';
 import { nanoid } from 'nanoid';
+import { db } from '@/firebaseApp';
+import { format } from 'date-fns';
 import { collection, getDocs, query, where } from 'firebase/firestore';
-import ThreeDotsLoading from '@/components/loading/ThreeDots';
 import { useAuth } from '@/hooks';
+import { dateFormat, dateWeekFormatter } from '@/utils/calendarUtil';
+
+import ThreeDotsLoading from '@/components/loading/ThreeDots';
+import HeaderBefore from '@/components/headerBefore/HeaderBefore';
+
 import 'react-calendar/dist/Calendar.css';
 import './calendarPage.scss';
-import {
-  dateFormat,
-  dateWeekFormatter,
-  hhmmFormat,
-  reactCalendarDayFormat,
-} from '@/utils/calendarUtil';
 
-import HeaderBefore from '@/components/headerBefore/HeaderBefore';
 import { CALENDAR_ICONS } from '@/constants/calendar';
 import { UserPlant } from '@/@types/plant.type';
-import { format } from 'date-fns';
 
-/**
- * firebase에서 받아온걸로 변경해야함
- * 샘플 데이터 START
- */
-const mockWaterValue: WaterRecordType[] = [
-  {
-    date: '2023-08-01',
-    icon: CALENDAR_ICONS[0],
-    list: [
-      { time: '09:00', plant: '쑥쑥이' },
-      { time: '10:00', plant: '유나의 스투시' },
-      { time: '11:00', plant: '랩몬스테라' },
-      { time: '12:00', plant: '식물 이름이 나오는 영역 입니다 입니다' },
-      { time: '13:00', plant: '식물 이름이 나오는 영역 입니다 입니다2' },
-      { time: '15:00', plant: '뀨뀨까까' },
-    ],
-  },
-  {
-    date: '2023-08-10',
-    icon: CALENDAR_ICONS[1],
-    list: [
-      { time: '10:00', plant: '유나의 스투시' },
-      { time: '11:00', plant: '랩몬스테라' },
-      { time: '12:00', plant: '식물 이름이 나오는 영역 입니다 입니다' },
-      { time: '13:00', plant: '식물 이름이 나오는 영역 입니다 입니다2' },
-      { time: '15:00', plant: '뀨뀨까까' },
-      { time: '15:00', plant: '뀨뀨까까' },
-      { time: '15:00', plant: '뀨뀨까까' },
-      { time: '15:00', plant: '뀨뀨까까' },
-    ],
-  },
-  {
-    date: '2023-08-16',
-    icon: CALENDAR_ICONS[2],
-    list: [
-      { time: '10:00', plant: '쑥쑥이' },
-      { time: '10:00', plant: '유나의 스투시' },
-      { time: '11:00', plant: '랩몬스테라' },
-      { time: '15:00', plant: '뀨뀨까까' },
-    ],
-  },
-  {
-    date: '2023-08-19',
-    icon: CALENDAR_ICONS[3],
-    list: [
-      { time: '09:00', plant: '쑥쑥이' },
-      { time: '10:00', plant: '쑥쑥이' },
-    ],
-  },
-  {
-    date: '2023-08-21',
-    icon: CALENDAR_ICONS[4],
-    list: [
-      { time: '19:00', plant: '쑥쑥이' },
-      { time: '20:00', plant: '쑥쑥이' },
-    ],
-  },
-  {
-    date: '2023-08-30',
-    icon: CALENDAR_ICONS[5],
-    list: [
-      { time: '10:00', plant: '쑥쑥이' },
-      { time: '15:00', plant: '쑥쑥이' },
-    ],
-  },
-  {
-    date: '2023-08-31',
-    icon: CALENDAR_ICONS[6],
-    list: [
-      { time: '10:00', plant: '쑥쑥이' },
-      { time: '15:00', plant: '쑥쑥이' },
-    ],
-  },
-];
+// 테스팅 중
+import { mockWaterValue } from '@/mock/calendarMock';
 
 type ValuePiece = Date | null;
 
@@ -105,7 +30,7 @@ interface RecordType {
   time: string;
   plant: string;
 }
-interface WaterRecordType {
+export interface WaterRecordType {
   icon: string;
   date: string;
   list: RecordType[];
@@ -117,7 +42,7 @@ const CalendarPage = () => {
   const user = useAuth();
   const [loading, setLoading] = useState(false);
 
-  // 캘린더 데이터 => waterRecords 로 변경
+  // calendarData => waterRecords 로 변경 예정
   const [calendarData, setCalendarData] =
     useState<WaterRecordType[]>(mockWaterValue);
   // 유저가 선택한 날짜
@@ -159,7 +84,6 @@ const CalendarPage = () => {
 
       querySnapshot.forEach(doc => {
         const plantData = doc.data() as Omit<UserPlant, 'id'>;
-        /* 형식 변환 필요 */
 
         const plantName = plantData.nickname;
 
@@ -207,7 +131,7 @@ const CalendarPage = () => {
             <section className="calendar_wrap inner">
               <Calendar
                 value={selectedDate}
-                formatDay={reactCalendarDayFormat}
+                formatDay={(_, date) => date.getDate().toString()}
                 calendarType="hebrew"
                 nextLabel=""
                 prevLabel=""
@@ -215,7 +139,6 @@ const CalendarPage = () => {
                 onChange={setSelectedDate}
               />
             </section>
-
             <section className="date_list_wrap inner">
               <strong className="date_title">
                 {selectedDate instanceof Date

4e57726b7456595c408a5f8a37526639b166748a
Park ChangHyub <thwlckd@naver.com>
2023-09-01 18:13:14 +0900

    Feat: 마이페이지 사용가이드 페이지 추가 - 기종별 pwa 사용 가이드 작성

--- src/constants/myPage.ts
+++ src/constants/myPage.ts
@@ -1,6 +1,14 @@
 const customerService = [
-  { title: '공지사항', url: '' },
-  { title: '자주 묻는 질문', url: '' },
+  // { title: '사용 가이드', url: 'https://plant-utopia.vercel.app/mypage/noti' },
+  { title: '사용 가이드', url: 'http://localhost:5173/mypage/guide' },
+  // {
+  //   title: '자주 묻는 질문',
+  //   url: 'https://plant-utopia.vercel.app/mypage/question',
+  // },
+  {
+    title: '자주 묻는 질문',
+    url: 'http://localhost:5173/mypage/question',
+  },
   { title: '식물 추가 요청', url: 'https://forms.gle/g4AjkNKqVDP48Xnc7' },
 ];
 
--- src/routes/myPageRoutes.tsx
+++ src/routes/myPageRoutes.tsx
@@ -1,11 +1,14 @@
 import { lazy } from 'react';
 
 const MyPage = lazy(() => import('@/pages/myPage/MyPage'));
-const MyInfoPage = lazy(() => import('@/pages/myPage/MyInfoPage'));
+const MyInfoPage = lazy(() => import('@/pages/myPage/myInfoPage/MyInfoPage'));
+const GuidePage = lazy(() => import('@/pages/myPage/guidePage/GuidePage'));
 
 const myPageRoutes = [
   { path: '/mypage', element: <MyPage /> },
   { path: '/mypage/info', element: <MyInfoPage /> },
+  { path: '/mypage/guide', element: <GuidePage /> },
+  // { path: '/mypage/question', element: <NotiPage /> },
 ];
 
 export default myPageRoutes;
